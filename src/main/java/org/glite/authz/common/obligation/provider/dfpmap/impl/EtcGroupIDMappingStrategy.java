6da2c169535c6ce9372c3066dfdfafa2cba46d7c
Chad La Joie <lajoie@itumi.biz>
2009-06-15 12:47:40 +0000

    make mapping strategies read /etc passwd and group directly

--- src/main/java/org/glite/authz/common/obligation/provider/dfpmap/impl/EtcGroupIDMappingStrategy.java
+++ src/main/java/org/glite/authz/common/obligation/provider/dfpmap/impl/EtcGroupIDMappingStrategy.java
@@ -13,22 +13,71 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.glite.authz.common.obligation.provider.dfpmap.impl;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.LineNumberReader;
+import java.util.HashMap;
 
 import org.glite.authz.common.obligation.ObligationProcessingException;
 import org.glite.authz.common.obligation.provider.dfpmap.IDMappingStrategy;
-import org.jruby.ext.posix.Group;
+import org.glite.authz.common.util.Files;
+import org.glite.authz.common.util.Strings;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /** Resolves a name against an /etc/group file and returns its GID. */
 public class EtcGroupIDMappingStrategy implements IDMappingStrategy {
 
+    /** Class logger. */
+    private final Logger log = LoggerFactory.getLogger(EtcPasswdIDMappingStrategy.class);
+
+    /** Map from group name to ID. */
+    private HashMap<String, Integer> map;
+
+    /** Constructor. */
+    public EtcGroupIDMappingStrategy() {
+        map = new HashMap<String, Integer>();
+        readEtcGroup();
+    }
+
     /** {@inheritDoc} */
     public Integer mapToID(String name) throws ObligationProcessingException {
-        Group group = PosixUtil.getGroupByName(name);
-        if(group != null){
-            return new Integer((int) group.getGID());
+        return map.get(name);
+    }
+
+    /** Reads the /etc/group file and loads it in to the map. */
+    private void readEtcGroup() {
+        File etcGroupFile = null;
+        try {
+            etcGroupFile = Files.getReadableFile("/etc/group");
+        } catch (IOException e) {
+            log.error(e.getMessage());
+            return;
         }
-        
-        return null;
+
+        try {
+            LineNumberReader etcGroupReader = new LineNumberReader(new FileReader(etcGroupFile));
+
+            String line = etcGroupReader.readLine();
+            String trimmedLine;
+            String[] entry;
+            while (line != null) {
+                trimmedLine = Strings.safeTrimOrNullString(line);
+                if (trimmedLine != null && !trimmedLine.startsWith("#")) {
+                    entry = trimmedLine.split(":");
+                    log.debug("/etc/group line {} maps group name {} to GID {}", new Object[] {etcGroupReader.getLineNumber(), entry[0], entry[2]});
+                    map.put(entry[0], new Integer(entry[2]));
+                }else{
+                    log.debug("Ignoring /etc/group line {} because it empty or a comment", etcGroupReader.getLineNumber());
+                }
+                line = etcGroupReader.readLine();
+            }
+        } catch (IOException e) {
+            log.error("Error while reading /etc/group file", e);
+        }
     }
 }
--- src/main/java/org/glite/authz/common/obligation/provider/dfpmap/impl/EtcPasswdIDMappingStrategy.java
+++ src/main/java/org/glite/authz/common/obligation/provider/dfpmap/impl/EtcPasswdIDMappingStrategy.java
@@ -16,20 +16,68 @@
 
 package org.glite.authz.common.obligation.provider.dfpmap.impl;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.LineNumberReader;
+import java.util.HashMap;
+
 import org.glite.authz.common.obligation.ObligationProcessingException;
 import org.glite.authz.common.obligation.provider.dfpmap.IDMappingStrategy;
-import org.jruby.ext.posix.Passwd;
+import org.glite.authz.common.util.Files;
+import org.glite.authz.common.util.Strings;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /** Resolves a name against an /etc/passwd file and returns its UID. */
 public class EtcPasswdIDMappingStrategy implements IDMappingStrategy {
 
+    /** Class logger. */
+    private final Logger log = LoggerFactory.getLogger(EtcPasswdIDMappingStrategy.class);
+
+    /** Map from login name to UID. */
+    private HashMap<String, Integer> map;
+
+    /** Constructor. */
+    public EtcPasswdIDMappingStrategy() {
+        map = new HashMap<String, Integer>();
+        readEtcPasswd();
+    }
+
     /** {@inheritDoc} */
     public Integer mapToID(String name) throws ObligationProcessingException {
-        Passwd account = PosixUtil.getAccountByName(name);
-        if (account != null) {
-            return new Integer((int) account.getUID());
-        }
+        return map.get(name);
+    }
 
-        return null;
+    /** Reads the /etc/passwd file and loads it in to the map. */
+    private void readEtcPasswd() {
+        File etcPasswdFile = null;
+        try {
+            etcPasswdFile = Files.getReadableFile("/etc/passwd");
+        } catch (IOException e) {
+            log.error(e.getMessage());
+            return;
+        }
+        
+        try {
+            LineNumberReader etcPasswdReader = new LineNumberReader(new FileReader(etcPasswdFile));
+
+            String line = etcPasswdReader.readLine();
+            String trimmedLine;
+            String[] entry;
+            while (line != null) {
+                trimmedLine = Strings.safeTrimOrNullString(line);
+                if (trimmedLine != null && !trimmedLine.startsWith("#")) {
+                    entry = trimmedLine.split(":");
+                    log.debug("/etc/passwd line {} maps login name {} to GID {}", new Object[] {etcPasswdReader.getLineNumber(), entry[0], entry[2]});
+                    map.put(entry[0], new Integer(entry[2]));
+                }else{
+                    log.debug("Ignoring /etc/passwd line {} because it empty or a comment", etcPasswdReader.getLineNumber());
+                }
+                line = etcPasswdReader.readLine();
+            }
+        } catch (IOException e) {
+            log.error("Error while reading /etc/passwd file", e);
+        }
     }
 }
