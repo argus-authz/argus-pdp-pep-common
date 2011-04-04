5de3c5576e2b0a5745ff6ffdbdafe9a3b5ebfcc7
Valery Tschopp <valery.tschopp@switch.ch>
2011-04-04 15:47:13 +0000

    disable ECDH ciphers because of the OpenSSL 1.0 client problem with SSL handshake.

--- src/main/java/org/glite/authz/common/http/JettySslSelectChannelConnector.java
+++ src/main/java/org/glite/authz/common/http/JettySslSelectChannelConnector.java
@@ -17,19 +17,30 @@
 
 package org.glite.authz.common.http;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import javax.net.ssl.KeyManager;
 import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLEngine;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509KeyManager;
 import javax.net.ssl.X509TrustManager;
 
 import org.mortbay.jetty.security.SslSelectChannelConnector;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
- * Any extension to the basic Jetty SSL connection handler that allows a pre-instantiated key and trust manager to be
- * used when create new SSL connections.
+ * Any extension to the basic Jetty SSL connection handler that allows a
+ * pre-instantiated key and trust manager to be used when create new SSL
+ * connections.
  */
 public class JettySslSelectChannelConnector extends SslSelectChannelConnector {
+
+    private Logger log= LoggerFactory.getLogger(JettySslSelectChannelConnector.class);
 
     /** {@link KeyManager} used by this TLS connector. */
     private X509KeyManager keyManager;
@@ -40,25 +51,55 @@ public class JettySslSelectChannelConnector extends SslSelectChannelConnector {
     /**
      * Constructor.
      * 
-     * @param key the key manager used for the TLS connections
-     * @param trust the trust manager used for the TLS connections
+     * @param key
+     *            the key manager used for the TLS connections
+     * @param trust
+     *            the trust manager used for the TLS connections
      */
-    public JettySslSelectChannelConnector(X509KeyManager key, X509TrustManager trust) {
-        if(key == null){
+    public JettySslSelectChannelConnector(X509KeyManager key,
+            X509TrustManager trust) {
+        super();
+
+        if (key == null) {
             throw new IllegalArgumentException("X.509 key manager may not be null");
         }
-        keyManager = key;
-        
-        if(trust == null){
+        keyManager= key;
+
+        if (trust == null) {
             throw new IllegalArgumentException("X.509 trust manager may not be null");
         }
-        trustManager = trust;
+        trustManager= trust;        
+    }
+
+    /**
+     * Disable the all ECDH cipher suites because of the OpenSSL 1.0 problem
+     * with SSL handshake.
+     * <p>
+     * {@inheritDoc}
+     */
+    protected SSLEngine createSSLEngine() throws IOException {
+        SSLEngine sslEngine= super.createSSLEngine();
+        String enabledCipherSuites[]= sslEngine.getEnabledCipherSuites();
+        List<String> cipherSuites= new ArrayList<String>(Arrays.asList(enabledCipherSuites));
+        for (String cipher : enabledCipherSuites) {
+            if (cipher.contains("ECDH")) {
+                log.debug("disabling cipher: {}", cipher);
+                cipherSuites.remove(cipher);
+            }
+        }
+        log.debug("enabling ciphers: {}", cipherSuites);
+        enabledCipherSuites= (String[]) cipherSuites.toArray(new String[cipherSuites.size()]);
+        sslEngine.setEnabledCipherSuites(enabledCipherSuites);
+        return sslEngine;
     }
 
     /** {@inheritDoc} */
     protected SSLContext createSSLContext() throws Exception {
-        SSLContext sslConext = SSLContext.getInstance("TLS");
-        sslConext.init(new KeyManager[] { keyManager }, new TrustManager[] { trustManager }, null);
+        SSLContext sslConext= SSLContext.getInstance("TLS");
+        sslConext.init(new KeyManager[] { keyManager },
+                       new TrustManager[] { trustManager },
+                       null);
         return sslConext;
     }
+
 }
